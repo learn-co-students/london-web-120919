5146d1e9efba913038e2c7f0c2e35eb204ffe8ed
github username <github email address>
2020-01-29 09:51:45 +0000

    Added JS Events

--- 20-js-events/script.js
+++ 20-js-events/script.js
@@ -1,23 +1,40 @@
+// Add an event listener to an element - the first argument we pass is a string of the type of event we want this element to listen for e.g. click, submit, keypress. The second argument is a function we want it to perform only when that event is fired. In this case, we tell the document to wait until the entire DOM is loaded before performing the function, which contains all of our code. In other words, we won't attempt to run any code which manipulates the DOM until it's finished loading.
 document.addEventListener("DOMContentLoaded", function(){
-
   const button = document.querySelector("#event-button")
+  // Add a click event listener to the button. The e parameter refers to the event that has been fired.
   button.addEventListener("click", function(e){
-    document.querySelector(".main-heading").style.fontFamily = "Comic Sans MS"
-    e.target.innerText = "Now doesn't that look nice????"
+    // Grab the h1 with id of main-heading and change its font-family to Comic Sans MS.
+    const mainHeading = document.querySelector(".main-heading")
+    mainHeading.style.fontFamily = "Comic Sans MS"
+    // Access the target that received the event (the button we clicked) via e.target so that we can then manipulate it.
+    e.target.innerText = "Doesn't that look better???"
   })
 
-  // Grab the form and add an event listener to it
-  document.querySelector("form").addEventListener("submit", function(e){
+  // We could also define a named fuction and pass this to the event listener as its argument, rather than an anonymous one.
+  function changeHeadingFont(e) {
+    const mainHeading = document.querySelector(".main-heading")
+    mainHeading.style.fontFamily = "Comic Sans MS"
+    e.target.innerText = "Doesn't that look better???"
+  }
+  button.addEventListener("click", changeHeadingFont)
+
+  const form = document.querySelector("form")
+  // Add a submit event listener to our form.
+  form.addEventListener("submit", function(e){
+    // This prevents an event's default behaviour. In this case, it stops the form from submitting a POST request and thus refreshing the page after it's submitted.
     e.preventDefault()
-    // The event listener function will:
-    // Grab the values of the name and age input fields
-    const name = document.querySelector('input[name="name"]').value
-    const age = document.querySelector('input[name="age"]').value
-    // Create a new p tag with the name and age separated by a dash
-    const p = document.createElement("p")
-    p.innerText = `${name} - ${age}`
-    // Add the new p tag to the students-list div
-    document.querySelector("#students-list").appendChild(p)
+    // Create a new p tag.
+    const newStudent = document.createElement("p")
+
+    // Grab the value of the name and age input fields.
+    const studentName = document.querySelector('input[name="name"]').value
+    const studentAge = document.querySelector('input[name="age"]').value
+
+    // Set the inner text of the new p tag to be the name and age that the user typed in.
+    newStudent.innerText = `${studentName} - ${studentAge}`
+
+    // Append the new p tag to the end of our students-list div.
+    document.querySelector("#students-list").appendChild(newStudent)
   })
 
 })
